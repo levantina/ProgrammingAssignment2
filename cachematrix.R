512610307dd003c678abb0cb247c97ec0179bac2
Valentina Biagini <vbiagini@gmail.com>
2015-03-11 20:27:51 +0100

    typo

--- cachematrix.R
+++ cachematrix.R
@@ -16,7 +16,7 @@ makeCacheMatrix <- function(x=matrix()) {
 		 getInverse = getInverse)
 }
 
-## cacheSolve calculates the inverse of that spacial matrix and
+## cacheSolve calculates the inverse of that special matrix and
 ## if the matrix inverse has already been calculated, cacheSolve will 
 ## find it in the cache and return it (to avoid calculating the inverse again)
 
